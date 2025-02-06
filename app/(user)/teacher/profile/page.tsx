035663bf5d9851b017870836f5d4807e6f033e3e
Saad Nisar Butt <83548173+saadbutt27@users.noreply.github.com>
2025-02-06 20:29:05 +0500

    Update: added a loader in profile

--- app/(user)/teacher/profile/page.tsx
+++ app/(user)/teacher/profile/page.tsx
@@ -171,10 +171,10 @@ export default function ProfilePage() {
       if (!res.ok) {
         if (res.status === 401) {
           setPassUpdated("incorrect");
-        } 
+        }
         return;
       }
-      
+
       const data = await res.json();
       if (data) {
         setPassUpdated("changed");
@@ -193,175 +193,189 @@ export default function ProfilePage() {
       user.t_image ||
       "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png";
     return (
-      <>
-        <div className="md:ml-[80px] m-4 px-2">
-          <div className="my-4 px-6 pt-2 pb-8 bg-white border-2 shadow-md h-auto flex flex-col items-start">
-            <h1 className="text-3xl text-left font-medium tracking-normal leading-snug select-none">
-              My LMS Profile
-            </h1>
-            <form
-              action=""
-              // onSubmit={(e) => handleUpdateProfile(e)}
-            >
+      <div className="md:ml-[80px] m-4 px-2">
+        <div className="my-4 px-6 pt-2 pb-8 bg-white border-2 shadow-md h-auto flex flex-col items-start">
+          <h1 className="text-3xl text-left font-medium tracking-normal leading-snug select-none">
+            My LMS Profile
+          </h1>
+          <form
+            action=""
+            // onSubmit={(e) => handleUpdateProfile(e)}
+          >
+            <div className="my-6 flex sm:flex-row flex-col sm:items-center sm:justify-between">
+              <label
+                htmlFor="stName"
+                className="mb-2 mr-16 text-base font-semibold text-gray-900"
+              >
+                Name{" "}
+              </label>
+              <input
+                type="text"
+                name="stName"
+                id="stName"
+                value={user.t_name}
+                className="w-full sm:w-2/3 bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-gray-500 focus:border-gray-500 p-2.5 cursor-not-allowed"
+                disabled
+              />
+            </div>
+            <div className="my-6 flex sm:flex-row flex-col sm:items-center sm:justify-between">
+              <label
+                htmlFor="stId"
+                className="mb-2 mr-16 text-base font-semibold text-gray-900"
+              >
+                Teacher ID{" "}
+              </label>
+              <input
+                type="text"
+                name="stId"
+                id="stId"
+                value={user.t_id}
+                className="w-full sm:w-2/3 bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-gray-500 focus:border-gray-500 p-2.5 cursor-not-allowed sel"
+                disabled
+              />
+            </div>
+            <div className="my-6 flex items-center ">
+              <label
+                htmlFor="prfPic"
+                className="mb-2 mr-16 text-base font-semibold text-gray-900"
+              >
+                Profile Picture{" "}
+              </label>
+              <div>
+                <Avatar className="mb-2 w-16 h-16 select-none">
+                  <AvatarImage
+                    src={src}
+                    className="object-center object-cover"
+                  />
+                  <AvatarFallback>Profile Picture</AvatarFallback>
+                </Avatar>
+                <div className="flex items-center"></div>
+                {user.t_image && (
+                  <button
+                    className="mb-6 bg-gray-900 hover:bg-gray-900 hover:scale-110 hover:duration-200 text-white w-auto font-medium rounded-md text-sm px-2 py-1"
+                    onClick={handleDeleteProfilePic}
+                  >
+                    Delete picture
+                  </button>
+                )}
+              </div>
+            </div>
+            <div className="my-6 flex sm:flex-row flex-col sm:items-center sm:justify-between">
+              <label
+                htmlFor="newPic"
+                className="mb-2 mr-16 text-base font-semibold text-gray-900"
+              >
+                New Picture{" "}
+              </label>
+              <div className="flex items-start gap-x-2">
+                <input onChange={uploadFile} type="file" disabled={uploading} />
+              </div>
+              {uploading && (
+                <p className="text-lg text-semibold">Uploading...</p>
+              )}
+            </div>
+          </form>
+          <button
+            className="mb-2 text-base font-medium text-gray-900 underline"
+            onClick={() => setPassword(!password)}
+          >
+            Reset Password
+          </button>
+          {password && (
+            <form action="" onSubmit={(e) => handlePasswordReset(e)}>
               <div className="my-6 flex sm:flex-row flex-col sm:items-center sm:justify-between">
                 <label
-                  htmlFor="stName"
-                  className="mb-2 mr-16 text-base font-semibold text-gray-900"
+                  htmlFor="oldPass"
+                  className="basis-2/5 mb-2 text-base font-semibold text-gray-900"
                 >
-                  Name{" "}
+                  Old Password{" "}
+                </label>
+                <div className="basis-3/5 w-full sm:w-2/3 sm:ml-6">
+                  <input
+                    type="password"
+                    name="oldPass"
+                    id="oldPass"
+                    className={`w-full ${
+                      passUpdated === "incorrect" ? "bg-red-300" : "bg-gray-50"
+                    } border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-gray-500 focus:border-gray-500 p-2.5`}
+                    required
+                  />
+                  {passUpdated === "incorrect" ? (
+                    <p className="text-red-600">Incorrect Password</p>
+                  ) : null}
+                </div>
+              </div>
+
+              <div className="my-6 flex sm:flex-row flex-col sm:items-center sm:justify-between">
+                <label
+                  htmlFor="newPass"
+                  className="basis-2/5 mb-2 text-base font-semibold text-gray-900"
+                >
+                  New Password{" "}
                 </label>
                 <input
-                  type="text"
-                  name="stName"
-                  id="stName"
-                  value={user.t_name}
-                  className="w-full sm:w-2/3 bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-gray-500 focus:border-gray-500 p-2.5 cursor-not-allowed"
-                  disabled
+                  type="password"
+                  name="newPass"
+                  id="newPass"
+                  className="basis-3/5 w-full sm:w-2/3 sm:ml-6 bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-gray-500 focus:border-gray-500 p-2.5"
+                  required
                 />
               </div>
               <div className="my-6 flex sm:flex-row flex-col sm:items-center sm:justify-between">
                 <label
-                  htmlFor="stId"
-                  className="mb-2 mr-16 text-base font-semibold text-gray-900"
+                  htmlFor="newPass"
+                  className="basis-2/5 mb-2 text-base font-semibold text-gray-900"
                 >
-                  Teacher ID{" "}
+                  Confirm Password{" "}
                 </label>
-                <input
-                  type="text"
-                  name="stId"
-                  id="stId"
-                  value={user.t_id}
-                  className="w-full sm:w-2/3 bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-gray-500 focus:border-gray-500 p-2.5 cursor-not-allowed sel"
-                  disabled
-                />
-              </div>
-              <div className="my-6 flex items-center ">
-                <label
-                  htmlFor="prfPic"
-                  className="mb-2 mr-16 text-base font-semibold text-gray-900"
-                >
-                  Profile Picture{" "}
-                </label>
-                <div>
-                  <Avatar className="mb-2 w-16 h-16 select-none">
-                    <AvatarImage
-                      src={src}
-                      className="object-center object-cover"
-                    />
-                    <AvatarFallback>Profile Picture</AvatarFallback>
-                  </Avatar>
-                  <div className="flex items-center"></div>
-                  {user.t_image && (
-                    <button
-                      className="mb-6 bg-gray-900 hover:bg-gray-900 hover:scale-110 hover:duration-200 text-white w-auto font-medium rounded-md text-sm px-2 py-1"
-                      onClick={handleDeleteProfilePic}
-                    >
-                      Delete picture
-                    </button>
+                <div className="basis-3/5 w-full sm:w-2/3 sm:ml-6">
+                  <input
+                    type="password"
+                    name="cnfNewPass"
+                    id="cnfNewPass"
+                    className="w-full bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-gray-500 focus:border-gray-500 p-2.5"
+                    required
+                  />
+                  {passUpdated === "changed" && (
+                    <p className="text-green-600">Password Changed ✔</p>
+                  )}
+                  {!passMatched && (
+                    <p className="text-red-600">
+                      Password doesn&apos;t matched ❌
+                    </p>
                   )}
                 </div>
               </div>
-              <div className="my-6 flex sm:flex-row flex-col sm:items-center sm:justify-between">
-                <label
-                  htmlFor="newPic"
-                  className="mb-2 mr-16 text-base font-semibold text-gray-900"
-                >
-                  New Picture{" "}
-                </label>
-                <div className="flex items-start gap-x-2">
-                  <input
-                    onChange={uploadFile}
-                    type="file"
-                    disabled={uploading}
-                  />
-                </div>
-                {uploading && (
-                  <p className="text-lg text-semibold">Uploading...</p>
-                )}
-              </div>
+              <button className="text-white bg-black hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">
+                Set password
+              </button>
             </form>
-            <button
-              className="mb-2 text-base font-medium text-gray-900 underline"
-              onClick={() => setPassword(!password)}
-            >
-              Reset Password
-            </button>
-            {password && (
-              <form action="" onSubmit={(e) => handlePasswordReset(e)}>
-                <div className="my-6 flex sm:flex-row flex-col sm:items-center sm:justify-between">
-                  <label
-                    htmlFor="oldPass"
-                    className="basis-2/5 mb-2 text-base font-semibold text-gray-900"
-                  >
-                    Old Password{" "}
-                  </label>
-                  <div className="basis-3/5 w-full sm:w-2/3 sm:ml-6">
-                    <input
-                      type="password"
-                      name="oldPass"
-                      id="oldPass"
-                      className={`w-full ${
-                        passUpdated === "incorrect"
-                          ? "bg-red-300"
-                          : "bg-gray-50"
-                      } border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-gray-500 focus:border-gray-500 p-2.5`}
-                      required
-                    />
-                    {passUpdated === "incorrect" ? (
-                      <p className="text-red-600">Incorrect Password</p>
-                    ) : null}
-                  </div>
-                </div>
-
-                <div className="my-6 flex sm:flex-row flex-col sm:items-center sm:justify-between">
-                  <label
-                    htmlFor="newPass"
-                    className="basis-2/5 mb-2 text-base font-semibold text-gray-900"
-                  >
-                    New Password{" "}
-                  </label>
-                  <input
-                    type="password"
-                    name="newPass"
-                    id="newPass"
-                    className="basis-3/5 w-full sm:w-2/3 sm:ml-6 bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-gray-500 focus:border-gray-500 p-2.5"
-                    required
-                  />
-                </div>
-                <div className="my-6 flex sm:flex-row flex-col sm:items-center sm:justify-between">
-                  <label
-                    htmlFor="newPass"
-                    className="basis-2/5 mb-2 text-base font-semibold text-gray-900"
-                  >
-                    Confirm Password{" "}
-                  </label>
-                  <div className="basis-3/5 w-full sm:w-2/3 sm:ml-6">
-                    <input
-                      type="password"
-                      name="cnfNewPass"
-                      id="cnfNewPass"
-                      className="w-full bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-gray-500 focus:border-gray-500 p-2.5"
-                      required
-                    />
-                    {passUpdated === "changed" && (
-                      <p className="text-green-600">Password Changed ✔</p>
-                    )}
-                    {!passMatched && (
-                      <p className="text-red-600">
-                        Password doesn&apos;t matched ❌
-                      </p>
-                    )}
-                  </div>
-                </div>
-                <button className="text-white bg-black hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">
-                  Set password
-                </button>
-              </form>
-            )}
-          </div>
+          )}
         </div>
-      </>
+      </div>
     );
   }
-  return <p>Loading...</p>;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div role="status">
+        <svg
+          aria-hidden="true"
+          className="w-10 h-10 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+          viewBox="0 0 100 101"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+            fill="currentColor"
+          />
+          <path
+            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+            fill="currentFill"
+          />
+        </svg>
+        <span className="sr-only">Loading...</span>
+      </div>
+    </div>
+  );
 }
